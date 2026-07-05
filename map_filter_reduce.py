aa2819f27fda1902af0c75e03ddc8cc36022d703
chaim <chchdeu@gmail.com>
2026-07-05 14:57:43 +0300

    mission_3

--- map_filter_reduce.py
+++ map_filter_reduce.py
@@ -6,3 +6,8 @@ print(list(map(lambda num : num +10,numbers)))
 prices = [100, 50, 200,80]
 prices_with_tax=map(lambda price : price * 1.17,prices)
 print(list(prices_with_tax))
+
+#mission 3
+words = ["cat", "elephant", "dog", "python"]
+langth=lambda word :len(word)
+print(list(map(langth,words)))
