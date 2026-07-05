ac9b802f566c329eef88e77a20fee65370ff5b40
chaim <chchdeu@gmail.com>
2026-07-05 14:55:36 +0300

    mission_2

--- map_filter_reduce.py
+++ map_filter_reduce.py
@@ -1,3 +1,8 @@
 #mission 1
 numbers=[3,7,10,15]
 print(list(map(lambda num : num +10,numbers)))
+
+#mission 2
+prices = [100, 50, 200,80]
+prices_with_tax=map(lambda price : price * 1.17,prices)
+print(list(prices_with_tax))
